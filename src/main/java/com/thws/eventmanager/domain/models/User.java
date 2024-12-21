4c4d533db0e0245750e31268e5a21097c5b0b79d
MaxGit <kelmaxov@gmail.com>
2024-12-21 15:35:48 +0100

    Artist create Event methode. EventLocation eingeführt. DBAdapter eingeführt.

--- src/main/java/com/thws/eventmanager/domain/models/User.java
+++ src/main/java/com/thws/eventmanager/domain/models/User.java
@@ -20,4 +20,39 @@ public class User {
             return email;
         }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
 }
