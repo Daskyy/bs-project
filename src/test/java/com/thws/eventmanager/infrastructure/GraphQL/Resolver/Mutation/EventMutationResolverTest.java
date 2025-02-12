c7f6590aeccc9c4ca8ac099dfd0c5923bd477516
MaxCods <kelmaxov+IT@gmail.com>
2025-02-12 16:37:47 +0100

    EventMutation full working

---
+++ src/test/java/com/thws/eventmanager/infrastructure/GraphQL/Resolver/Mutation/EventMutationResolverTest.java
@@ -0,0 +1,21 @@
+package com.thws.eventmanager.infrastructure.GraphQL.Resolver.Mutation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventMutationResolverTest {
+
+    @Test
+    void createEvent() {
+
+    }
+
+    @Test
+    void updateEvent() {
+    }
+
+    @Test
+    void deleteEvent() {
+    }
+}
