dd000a76adfbbd2b1cbe94665b7651edcef7f5ef
Dasky <daskymail@gmail.com>
2025-02-13 13:13:13 +0100

    searchByCriteria can now search for relationship ids in the format like "addressentity_id" in graphql, see criteria input for more details + eventlocationqueries

--- src/main/java/com/thws/eventmanager/domain/port/in/EventLocationServiceInterface.java
+++ src/main/java/com/thws/eventmanager/domain/port/in/EventLocationServiceInterface.java
@@ -1,4 +1,19 @@
 package com.thws.eventmanager.domain.port.in;
 
-public class EventLocationServiceInterface {
+import com.thws.eventmanager.domain.models.EventLocation;
+import com.thws.eventmanager.infrastructure.components.persistence.entities.EventLocationEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EventLocationServiceInterface {
+    EventLocationEntity saveLocation(EventLocation location);
+
+    Optional<EventLocationEntity> deleteLocation(EventLocation location);
+
+    Optional<EventLocationEntity> getLocationById(long id);
+
+    List<EventLocationEntity> getAllEventLocations();
+
+    List<EventLocationEntity> getAllEventLocations(List<String> criteria, List<Object> values);
 }
---
+++ src/main/java/com/thws/eventmanager/infrastructure/GraphQL/Resolver/Query/EventLocationQueryResolver.java
@@ -0,0 +1,50 @@
+package com.thws.eventmanager.infrastructure.GraphQL.Resolver.Query;
+
+import com.thws.eventmanager.domain.services.db.EventLocationService;
+import com.thws.eventmanager.infrastructure.GraphQL.InputModels.EventLocationCriteriaInput;
+import com.thws.eventmanager.infrastructure.GraphQL.Models.EventLocationGQL;
+import com.thws.eventmanager.infrastructure.GraphQL.Resolver.Mapper.MapperGQLDomain.EventLocationMapperGQL;
+import com.thws.eventmanager.infrastructure.components.persistence.entities.EventLocationEntity;
+import com.thws.eventmanager.infrastructure.components.persistence.mapper.EventLocationMapper;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+
+import java.util.List;
+
+public class EventLocationQueryResolver implements GraphQLQueryResolver {
+    EventLocationMapper mapper = new EventLocationMapper();
+    EventLocationMapperGQL mapperGQL = new EventLocationMapperGQL();
+    EventLocationService eventLocationService = new EventLocationService();
+    /*
+        String id="-1";
+    AddressInput address;
+    String name;
+    int capacity;
+
+     */
+
+    public EventLocationGQL eventLocation(String id){
+        EventLocationEntity eventLocationEntity = eventLocationService.getLocationById(Long.parseLong(id)).orElse(null);
+        if(eventLocationEntity == null) return null;
+        return mapperGQL.toModelGQL(mapper.toModel(eventLocationEntity));
+    }
+
+    public List<EventLocationGQL> eventLocations(EventLocationCriteriaInput criteria){
+        if (criteria == null) {
+            return eventLocationService.getAllEventLocations().stream()
+                    .map(mapper::toModel)
+                    .map(mapperGQL::toModelGQL)
+                    .toList();
+        }
+
+        List<String> criteriaList = criteria.getCriteria();
+        List<Object> valuesList = criteria.getValues();
+        if (criteriaList.size() != valuesList.size()) {
+            throw new IllegalArgumentException("Criteria and values lists must have the same size");
+        } else {
+            return eventLocationService.getAllEventLocations(criteriaList, valuesList).stream()
+                    .map(mapper::toModel)
+                    .map(mapperGQL::toModelGQL)
+                    .toList();
+        }
+    }
+}
